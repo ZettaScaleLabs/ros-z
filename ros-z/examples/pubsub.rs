a23449466dab784ee6c59ef77929d63de971d4f0
yuanyuyuan <az6980522@gmail.com>
2025-07-21 18:53:07 +0800

    fix: use timeout to terminate the test

--- ros-z/examples/pubsub.rs
+++ ros-z/examples/pubsub.rs
@@ -19,46 +19,72 @@ struct Args {
     mode: String,
 }
 
-
 fn run_subscriber(topic: String, duration: Duration) -> Result<()> {
     let ctx = ZContextBuilder::default().build()?;
     let node = ctx.create_node("MyNode").build()?;
     let zsub = node.create_sub::<ByteMultiArray>(&topic).build()?;
-    let now = std::time::Instant::now();
-    loop {
-        let msg = zsub.recv()?;
-        tracing::info!("Recv {msg:?}");
-        if !duration.is_zero() && now.elapsed() >= duration {
-            break
+
+    if duration.is_zero() {
+        loop {
+            let msg = zsub.recv()?;
+            tracing::info!("Recv {msg:?}");
         }
+    } else {
+        let fut = async {
+            loop {
+                match zsub.async_recv().await {
+                    Ok(msg) => {
+                        tracing::info!("Recv {msg:?}");
+                    }
+                    Err(err) => {
+                        tracing::error!(err);
+                        break;
+                    }
+                }
+            }
+        };
+        tokio::runtime::Runtime::new()?.block_on(async {
+            let _ = tokio::time::timeout(duration, fut).await?;
+            Result::Ok(())
+        })?;
     }
+
     Ok(())
 }
 
-fn run_publisher(topic: String, duration: Duration, period: Duration, payload_size: usize) -> Result<()> {
+fn run_publisher(
+    topic: String,
+    duration: Duration,
+    period: Duration,
+    payload_size: usize,
+) -> Result<()> {
     let ctx = ZContextBuilder::default().build()?;
     let node = ctx.create_node("MyNode").build()?;
     let zpub = node.create_pub::<ByteMultiArray>(&topic).build()?;
     let now = std::time::Instant::now();
     let mut msg = ros_z::ros_msg::ByteMultiArray::default();
-    msg.data = vec![0u8, payload_size as _];
+    msg.data = vec![0u8; payload_size as _];
     loop {
         zpub.publish(&msg)?;
         std::thread::sleep(period);
         if !duration.is_zero() && now.elapsed() >= duration {
-            break
+            break;
         }
     }
     Ok(())
 }
-
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let duration = std::time::Duration::from_secs_f64(args.duration);
     let period = std::time::Duration::from_secs_f64(1.0 / args.rate as f64);
     zenoh::init_log_from_env_or("error");
-    tracing::info!("Begin testing with topic: {}, duration: {duration:?}, payload: {}, rate: {} Hz", args.topic, args.payload, args.rate);
+    tracing::info!(
+        "Begin testing with topic: {}, duration: {duration:?}, payload: {}, rate: {} Hz",
+        args.topic,
+        args.payload,
+        args.rate
+    );
     if args.mode == "sub" {
         run_subscriber(args.topic, duration)?;
     } else if args.mode == "pub" {
