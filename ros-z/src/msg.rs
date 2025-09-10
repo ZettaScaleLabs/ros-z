ed380154d41a24a730942544f13c78a8644c2540
yuanyuyuan <az6980522@gmail.com>
2025-09-10 15:20:34 +0800

    fix: cdr-encoding assumes no encapsulation is used

--- ros-z/src/msg.rs
+++ ros-z/src/msg.rs
@@ -36,7 +36,8 @@ where
     // FIXME: Use a result type
     type Output = T;
     fn deserialize(input: Self::Input<'_>) -> Self::Output {
-        let x = cdr_encoding::from_bytes::<T, byteorder::LittleEndian>(&input).unwrap();
+        // Skip the first four bytes
+        let x = cdr_encoding::from_bytes::<T, byteorder::LittleEndian>(&input[4..]).unwrap();
         x.0
     }
 }
@@ -47,7 +48,14 @@ where
 {
     type Input<'a> = &'a T;
     fn serialize(input: &T) -> Vec<u8> {
-         cdr_encoding::to_vec::<T, byteorder::LittleEndian>(input).unwrap()
+        // Allocate with enough space: 4 bytes header + payload
+        let mut buffer = Vec::with_capacity(std::mem::size_of_val(input) * 2 + 4);
+        // Write the encapsulation header (CDR LE = 0x00 01 00 00)
+        buffer.extend_from_slice(&[0x00, 0x01, 0x00, 0x00]);
+        // Serialize directly into the same buffer (appends after the header)
+        cdr_encoding::to_writer::<T, byteorder::LittleEndian, _>(&mut buffer, input).unwrap();
+
+        buffer
     }
 }
 
