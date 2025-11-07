5a7c6f89b3b77dd8849e718cdff9b581f1e89dcb
yuanyuyuan <az6980522@gmail.com>
2025-11-07 18:47:11 +0800

    fix: resolve compilation errors in message serialization traits
    - Add lifetime bounds to ZSerializer Input types (T: 'a)
    - Fix CdrSerdes to use byteorder::LittleEndian for CDR serialization
    - Fix CdrSerdes deserialization to extract tuple result (.0)
    - Add blanket ZMessage implementation for all serde-compatible types
    - Fix ProtobufSerdes lifetime bounds for feature-gated code
    - Remove incorrect #[macro_export] from utils.rs import

--- ros-z/src/msg.rs
+++ ros-z/src/msg.rs
@@ -1,7 +1,13 @@
 use std::marker::PhantomData;
+use serde::{Serialize, Deserialize};
+use byteorder::LittleEndian;
+#[cfg(feature = "protobuf")]
+use prost::Message as ProstMessage;
 
-use cdr::{CdrLe, Infinite};
-use serde::{Deserialize, Serialize};
+pub trait ZSerializer {
+    type Input<'a> where Self: 'a;
+    fn serialize(input: Self::Input<'_>) -> Vec<u8>;
+}
 
 pub trait ZDeserializer {
     type Input<'a>;
@@ -9,53 +15,85 @@ pub trait ZDeserializer {
     fn deserialize(input: Self::Input<'_>) -> Self::Output;
 }
 
-pub trait ZSerializer {
-    type Input<'a>;
-    fn serialize(input: Self::Input<'_>) -> Vec<u8>;
-}
-
-pub trait ZMessage {
+// Core Z-Message trait
+pub trait ZMessage: Sized {
     type Serdes: for<'a> ZSerializer<Input<'a> = &'a Self> + ZDeserializer;
+
     fn serialize(&self) -> Vec<u8> {
         Self::Serdes::serialize(self)
     }
 
-    fn deserialize(
-        input: <Self::Serdes as ZDeserializer>::Input<'_>,
-    ) -> <Self::Serdes as ZDeserializer>::Output {
+    fn deserialize(input: <Self::Serdes as ZDeserializer>::Input<'_>) -> Self
+    where
+        Self::Serdes: ZDeserializer<Output = Self>,
+    {
         Self::Serdes::deserialize(input)
     }
 }
 
+// Blanket implementation for serde-compatible types using CDR
+impl<T> ZMessage for T
+where
+    T: Serialize + for<'a> Deserialize<'a> + 'static,
+{
+    type Serdes = CdrSerdes<T>;
+}
+
+// CDR
+
 pub struct CdrSerdes<T>(PhantomData<T>);
+
+impl<T> ZSerializer for CdrSerdes<T>
+where
+    T: Serialize,
+{
+    type Input<'a> = &'a T where T: 'a;
+
+    fn serialize(input: &T) -> Vec<u8> {
+        cdr_encoding::to_vec::<T, LittleEndian>(input).unwrap()
+    }
+}
 
 impl<T> ZDeserializer for CdrSerdes<T>
 where
     for<'a> T: Deserialize<'a>,
 {
-    type Input<'a> = &'a [u8];
-    // FIXME: Use a result type
+    type Input<'b> = &'b [u8];
     type Output = T;
-    fn deserialize(input: Self::Input<'_>) -> Self::Output {
-        cdr::deserialize::<T>(input).unwrap()
+
+    fn deserialize(input: Self::Input<'_>) -> T {
+        cdr_encoding::from_bytes::<T, LittleEndian>(input).unwrap().0
     }
 }
 
-impl<T> ZSerializer for CdrSerdes<T>
+// Protobuf
+
+#[cfg(feature = "protobuf")]
+pub struct ProtobufSerdes<T>(PhantomData<T>);
+
+#[cfg(feature = "protobuf")]
+impl<T> ZSerializer for ProtobufSerdes<T>
 where
-    for<'a> T: Serialize + 'a,
+    T: ProstMessage,
 {
-    type Input<'a> = &'a T;
+    type Input<'a> = &'a T where T: 'a;
+
     fn serialize(input: &T) -> Vec<u8> {
-        cdr::serialize::<_, _, CdrLe>(input, Infinite).unwrap()
+        input.encode_to_vec()
     }
 }
 
-impl<T> ZMessage for T
+#[cfg(feature = "protobuf")]
+impl<T> ZDeserializer for ProtobufSerdes<T>
 where
-    for<'a> T: Serialize + Deserialize<'a> + 'a,
+    T: ProstMessage + Default,
 {
-    type Serdes = CdrSerdes<T>;
+    type Input<'a> = &'a [u8];
+    type Output = T;
+
+    fn deserialize(input: &[u8]) -> T {
+        T::decode(input).unwrap_or_default()
+    }
 }
 
 pub trait ZService {
